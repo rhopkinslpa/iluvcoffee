89b1466618523bcce35efe30706ef1cda690e1e7
rhopkinslpa <rhopkins@gmail.com>
2022-10-19 18:54:13 +0100

    Adding Pagination chapter

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, 
       forbidNonWhitelisted: true,
-      transform: true 
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
     }),
   );
   await app.listen(3000);
